eb04f6e316e2ec50dd0fc68287c0f8d503074fcc
Felipe Zepeda <pipe.vash@gmail.com>
2023-08-17 18:06:15 -0400

    app.py modification for the new NN model

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
+# Inicializamos las librerias necesarias 
 import pickle
 import json
 from flask import Flask, request
 import pandas as pd
+from keras import models
 
 
-FEATURES = pickle.load(open("churn/models/features.pk", "rb"))
-
-model = pickle.load(open("churn/models/model.pk", "rb"))
-column_equivalence = pickle.load(open("churn/models/column_equivalence.pk", "rb"))
+# Cargamos el modelo y los objetos necesarios para la transformacion de los datos
+model= models.load_model('churn/models/best_model.hdf5')
+categorical_transformer = pickle.load(open("churn/models/cat_trans.pk", "rb"))
+scaler = pickle.load(open("churn/models/scaler.pk", "rb"))
+df_features = pickle.load(open("churn/models/df_features.pk", "rb"))
 
 # create the Flask app
 app = Flask(__name__)
 
-def convert_numerical(features):
-    output = []
-    for i, feat in enumerate(features):
-        if i in column_equivalence:
-            output.append(column_equivalence[i][feat])
-        else:
-            try:
-                output.append(pd.to_numeric(feat))
-            except:
-                output.append(0)
-    return output
 
+# Hacemos la predicción y definimos la ruta para la API
 @app.route('/query')
 def query_example():
-    features = convert_numerical(request.args.get('feats').split(','))
+    feats = request.args.get('feats').split(',')
+    feats_df= pd.DataFrame([feats], columns= df_features)
+    final_df= scaler.transform(categorical_transformer.transform(feats_df))
+    test_pred = model.predict(final_df)
+    test_pred_labels = (test_pred > 0.5).astype(int).flatten()
+
+
     response = {
-        'response': [int(x) for x in model.predict([features])]
+        'response': test_pred_labels.tolist()
     }
     return json.dumps(response)
 
+# Corremos el programa.
 if __name__ == '__main__':
     # run app in debug mode on port 3001
     app.run(debug=True, port=3001)
